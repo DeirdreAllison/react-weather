dd9ab8b4594bfc8e2312752f6ac7b3e12be31513
Deirdre Allison <dallison@hollandamericagroup.com>
2017-03-23 10:26:45 -0700

    Remove unneeded file from create-react-app, add component to test

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+      </div>
+    );
+  }
+}
+
+export default Footer;
