9bdd840d9fb7e0cfb55c0e74e8558df807d61752
Deirdre Allison <dallison@hollandamericagroup.com>
2017-03-31 14:41:27 -0700

    Attempt to map icon names, not working yet

--- src/components/WeatherTileList/WeatherTileList.js
+++ src/components/WeatherTileList/WeatherTileList.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import WeatherIcons from 'react-weathericons';
 
-// clear-day: day-sunny
-// clear-night: night-clear
-// rain: wi-rain
-// snow: wi-snow
-// sleet: wi-sleet
-// wind: wi-cloudy-windy
-// fog: wi-fog
-// cloudy: wi-cloudy
-// partly-cloudy-day: day-sunny-overcast
-// partly-cloudy-night: night-alt-cloudy
-// default: na
-
 class WeatherTileList extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.getIconName = this.getIconName().bind(this);
+  // }
 
   render() {
     return (
@@ -24,15 +16,66 @@ class WeatherTileList extends Component {
   }
 
   renderWeatherTile({summary, icon, temperatureMax, temperatureMin, time}) {
+    renderWeatherIcon(icon) {
+      return <WeatherIcons name="day-sunny" size="2x" />
+      // switch(icon) {
+      //   case "clear-day":
+      //     return <WeatherIcons name="day-sunny" size="2x" />;
+      //     break;
+      //   default:
+      //     return <WeatherIcons name="na" size="2x" />;
+      //     break;
+      // }
+      }
     return (
       <div key={time}>
-      <WeatherIcons name="day-cloudy" size="2x" />
+        {this.renderWeatherIcon({icon})}
+      {/*<WeatherIcons name={this.getIconName({icon})} size="2x" />*/}
         <p>{summary}</p>
         <p>Low: {temperatureMin} <WeatherIcons name="degrees" size="2x" /></p>
         <p>High: {temperatureMax} <WeatherIcons name="degrees" size="2x" /></p>
       </div>
       )
     }
+
+
+
+    // getIconName({icon}) {
+    //   switch(icon) {
+    //     case "clear-day":
+    //       return "day-sunny";
+    //       break;
+    //     case "clear-night":
+    //       return "night-clear";
+    //       break;
+    //     case "rain":
+    //       return "wi-rain";
+    //       break;
+    //     case "snow":
+    //       return "wi-snow";
+    //       break;
+    //     case "sleet":
+    //       return "wi-sleet";
+    //       break;
+    //     case "wind":
+    //       return "wi-cloudy-windy";
+    //       break;
+    //     case "fog":
+    //       return "wi-fog";
+    //       break;
+    //     case "cloudy":
+    //       return "wi-cloudy";
+    //       break;
+    //     case "partly-cloudy-day":
+    //       return "day-sunny-overcast";
+    //       break;
+    //     case "partly-cloudy-night":
+    //       return "night-alt-cloudy";
+    //       break;
+    //     default:
+    //       return "na";
+    //   }
+    // }
 }
 
 export default WeatherTileList;
